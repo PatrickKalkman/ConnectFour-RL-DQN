b04ad1c6ea2738da4115606f365277a035050001
Patrick Kalkman <patrick@simpletechture.nl>
2024-11-08 10:25:23 +0100

    Added first class and tests for it also added Github workflow file

--- src/sparse_matrix/train.py
+++ src/sparse_matrix/train.py
@@ -1,5 +1,5 @@
-from pettingzoo.classic import connect_four_v3
 import numpy as np
+from pettingzoo.classic import connect_four_v3
 
 env = connect_four_v3.env(render_mode="human")
 env.reset()
@@ -13,7 +13,7 @@ for agent in env.agent_iter():
         # The observation space is a 6x7x2 array
         # Channel 0: positions of the current player's pieces (1s)
         # Channel 1: positions of the opponent's pieces (1s)
-        mask = observation['action_mask']
+        mask = observation["action_mask"]
         # Choose a random valid action
         valid_actions = [i for i in range(len(mask)) if mask[i] == 1]
         action = np.random.choice(valid_actions)
